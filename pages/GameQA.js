af24290e748ea3ec919c58c8562fee1571cac1e5
Hnonhnon <32641580+Hnonhnon@users.noreply.github.com>
2017-10-19 19:36:52 +0700

    creat GameQA

---
+++ pages/GameQA.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const Page = styled.div``
+const Question = styled.h2``
+const Input = styled.input``
+const Button = styled.button``
+
+
+const GameQA =() =>(
+    <Page>
+       <Question>HelloText</Question>
+       <Input/>
+       <Button>ตอบ</Button>
+    </Page>
+)
+
+export default GameQA
