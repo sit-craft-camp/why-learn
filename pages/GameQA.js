6695a382ef6e85ac25c7fb320f73744292021c69
Hnonhnon <32641580+Hnonhnon@users.noreply.github.com>
2017-10-19 19:56:27 +0700

    Weed

--- pages/GameQA.js
+++ pages/GameQA.js
@@ -2,16 +2,26 @@ import React from 'react'
 import styled from 'styled-components'
 
 
-const Page = styled.div``
+const Page = styled.div`
+
+display: flex;
+align-items: center;
+justify-content:space-around;
+flex-direction:column;
+min-height:100vh;`
+
+
 const Question = styled.h2``
-const Input = styled.input``
+const Input = styled.button``
 const Button = styled.button``
 
 
 const GameQA =() =>(
     <Page>
        <Question>HelloText</Question>
-       <Input/>
+       {[...Array(4)].map((_, index) => (
+           <Input key={index}>Choice {index+1}</Input>
+       ))}
        <Button>ตอบ</Button>
     </Page>
 )
