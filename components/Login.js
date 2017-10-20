87c6ebda042b38660a2d2c972122962c83d7904d
pipe34150 <pipe34150@hotmail.com>
2017-10-20 10:56:08 +0700

    add login

--- components/Login.js
+++ components/Login.js
@@ -20,4 +20,24 @@ export default class Login extends Component {
       </Button>
     </div>
   )
+  <script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '{your-app-id}',
+      cookie     : true,
+      xfbml      : true,
+      version    : '{latest-api-version}'
+    });
+    FB.AppEvents.logPageView();   
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
+
 }
