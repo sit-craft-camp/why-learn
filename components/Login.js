272cc1680f5a4bf373f5a1375ff38fb59f29850b
pipe34150 <pipe34150@hotmail.com>
2017-10-20 12:36:28 +0700

    Add Login Button

--- components/Login.js
+++ components/Login.js
@@ -34,13 +34,13 @@ export default class Login extends Component {
     });
   };
 
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = 'https://connect.facebook.net/th_TH/sdk.js#xfbml=1&version=v2.10&appId=153854811888809';
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
 
    function statusChangeCallbac(response){
     if(response.status === 'connected'){
@@ -62,6 +62,10 @@ export default class Login extends Component {
       console.log('User cancelled login or did not fully authorize.');
     }
   }, {scope: ''});
-  
+
   </script>
+
+  const loginButton = {
+    <div class="fb-login-button" data-max-rows="1" data-size="large" data-button-type="continue_with" data-show-faces="false" data-auto-logout-link="false" data-use-continue-as="false"></div>
+  }
 }
