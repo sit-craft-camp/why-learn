f114d664d5174efb2e5ec31c875dc9cd8860e79f
pipe34150 <pipe34150@hotmail.com>
2017-10-20 11:29:51 +0700

    add Token

--- components/Login.js
+++ components/Login.js
@@ -23,12 +23,15 @@ export default class Login extends Component {
   <script>
   window.fbAsyncInit = function() {
     FB.init({
-      appId      : '{your-app-id}',
+      appId      : '153854811888809',
       cookie     : true,
       xfbml      : true,
-      version    : '{latest-api-version}'
+      version    : 'v2.10'
     });
-    FB.AppEvents.logPageView();   
+
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+    });
   };
 
   (function(d, s, id){
@@ -38,6 +41,27 @@ export default class Login extends Component {
      js.src = "//connect.facebook.net/en_US/sdk.js";
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));
-</script>
 
+   function statusChangeCallbac(response){
+    if(response.status === 'connected'){
+      console.log('Logged in and authenticated');
+    }
+    else {
+      console.log('Not authenticated');
+    }
+   }
+
+   FB.login(function(response) {
+    if (response.authResponse) {
+      var access_token =   FB.getAuthResponse()['accessToken'];
+      console.log('Access Token = '+ access_token);
+      FB.api('/me', function(response) {
+      console.log('Good to see you, ' + response.name + '.');
+      });
+    } else {
+      console.log('User cancelled login or did not fully authorize.');
+    }
+  }, {scope: ''});
+  
+  </script>
 }
