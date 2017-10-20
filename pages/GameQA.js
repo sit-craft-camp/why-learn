27deefee9afbc371db9ef2b555af9f2dcc8c6522
Hnonhnon <32641580+Hnonhnon@users.noreply.github.com>
2017-10-20 14:53:09 +0700

    Updated GameQA

--- pages/GameQA.js
+++ pages/GameQA.js
@@ -8,21 +8,23 @@ display: flex;
 align-items: center;
 justify-content:space-around;
 flex-direction:column;
-min-height:100vh;`
+min-height:100vh;
+background-color:#b3ecff;`
 
 
 const Question = styled.h2``
 const Input = styled.button``
 const Button = styled.button``
 
+const choices = ['1.ไก่กับไข่อันไหนเกิดก่อน','B','C','D']
 
 const GameQA =() =>(
     <Page>
-       <Question>HelloText</Question>
-       {[...Array(4)].map((_, index) => (
-           <Input key={index}>Choice {index+1}</Input>
+       <Question>คำถามโลกแตก</Question>
+       {choices.map((choice, index) => (
+           <Input key={index}>{choice}</Input>
        ))}
-       <Button>ตอบ</Button>
+       
     </Page>
 )
 
